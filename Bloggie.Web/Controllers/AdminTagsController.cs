f639e88d5038c53388a9e9972d7a1779af41ef4c
Milind Khamkar <fullstackwithmilind@gmail.com>
2024-01-05 15:46:53 +0530

    crud using EF

--- Bloggie.Web/Controllers/AdminTagsController.cs
+++ Bloggie.Web/Controllers/AdminTagsController.cs
@@ -2,6 +2,7 @@
 using Bloggie.Web.Models.Domain;
 using Bloggie.Web.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bloggie.Web.Controllers
 {
@@ -21,7 +22,7 @@ namespace Bloggie.Web.Controllers
 
         [HttpPost]
         [ActionName("Add")]
-        public IActionResult Add(AddTagRequest addTagRequest)
+        public async Task<IActionResult> Add(AddTagRequest addTagRequest)
         {
             // incoming request
             //string name = Request.Form["name"];
@@ -29,17 +30,99 @@ namespace Bloggie.Web.Controllers
             // model binding
             //var name = addTagRequest.Name;
 
-            // converting add req into a domain model
             var tag = new Tag()
             {
                 Name = addTagRequest.Name,
                 DisplayName = addTagRequest.DisplayName
             };
 
-            bloggieDbContext.Tags.Add(tag);
-            bloggieDbContext.SaveChanges(); // insert into db
+            await bloggieDbContext.Tags.AddAsync(tag);
+            await bloggieDbContext.SaveChangesAsync();
 
-            return View("Add");
+            return RedirectToAction("List");
+        }
+
+        [HttpGet]
+        [ActionName("List")]
+        public async Task<IActionResult> List()
+        {
+            // using bd context to read all the tags
+            var tags = await bloggieDbContext.Tags.ToListAsync();
+
+            return View(tags);
+        }
+
+        [HttpGet]
+        [ActionName("Edit")]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            // name of i/p parameter must be same s asp-route-id tag helper
+
+            // 1st method
+            //bloggieDbContext.Tags.Find(id);
+
+            // 2nd method
+            var tag = await bloggieDbContext.Tags.FirstOrDefaultAsync(x => x.ID == id);
+
+            if(tag != null) 
+            {
+                var editTagRequest = new EditTagRequest()
+                {
+                    ID = tag.ID,
+                    Name = tag.Name,  
+                    DisplayName = tag.DisplayName,
+                };
+                return View(editTagRequest);
+            }
+
+            return View(null);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(EditTagRequest editTagRequest)
+        {
+            var tag = new Tag()
+            {
+                ID = editTagRequest.ID,
+                Name  = editTagRequest.Name,
+                DisplayName = editTagRequest.DisplayName,
+            };
+
+            var existingTag = await bloggieDbContext.Tags.FindAsync(tag.ID);
+
+            if(existingTag != null)
+            {
+                existingTag.Name = tag.Name;
+                existingTag.DisplayName = tag.DisplayName;
+
+                // updating existing tag
+                await bloggieDbContext.SaveChangesAsync();
+
+                // showing sucess notification
+                return RedirectToAction("Edit", new { id = editTagRequest.ID });
+            }
+
+            // showing failure notification
+            return RedirectToAction("Edit", new { id = editTagRequest.ID });
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public async Task<IActionResult> Delete(EditTagRequest editTagRequest)
+        {
+            var tag = await bloggieDbContext.Tags.FindAsync(editTagRequest.ID);
+
+            if(tag != null )
+            {
+                bloggieDbContext.Tags.Remove(tag);
+                await bloggieDbContext.SaveChangesAsync();
+
+                // show success notification
+                return RedirectToAction("List");
+            }
+
+            // showing error nitification
+            return RedirectToAction("Edit", new { id = editTagRequest.ID });
         }
     }
 }
---
+++ Bloggie.Web/Models/ViewModels/EditTagRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Bloggie.Web.Models.ViewModels
+{
+    public class EditTagRequest
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+    }
+}
