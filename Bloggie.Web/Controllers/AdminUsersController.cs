86307a7396f3036666f4554421c598468e313cd3
Milind Khamkar <fullstackwithmilind@gmail.com>
2024-02-14 08:12:59 +0530

    add user functionality

--- Bloggie.Web/Controllers/AdminUsersController.cs
+++ Bloggie.Web/Controllers/AdminUsersController.cs
@@ -1,7 +1,9 @@
 ﻿using Bloggie.Web.Models.ViewModels;
 using Bloggie.Web.Repository;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
 
 namespace Bloggie.Web.Controllers
 {
@@ -9,14 +11,16 @@ namespace Bloggie.Web.Controllers
     public class AdminUsersController : Controller
     {
         private readonly IUserRepository userRepository;
+        private readonly UserManager<IdentityUser> userManager;
 
-        public AdminUsersController(IUserRepository userRepository)
+        public AdminUsersController(IUserRepository userRepository, UserManager<IdentityUser> userManager)
         {
             this.userRepository = userRepository;
+            this.userManager = userManager;
         }
 
 
-
+        [HttpGet]
         public async Task<IActionResult>  List()
         {
             var users = await userRepository.GetAll();
@@ -36,6 +40,42 @@ namespace Bloggie.Web.Controllers
             }
 
             return View(userViewModel);
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> List(UserViewModel userViewModel)
+        {
+            var identityUser = new IdentityUser()
+            {
+                UserName = userViewModel.Username,
+                Email  = userViewModel.Email,
+            };
+
+            var identityResult = await userManager.CreateAsync(identityUser, userViewModel.Password);
+
+            if(identityResult is not null)
+            {
+                if (identityResult.Succeeded)
+                {
+                    // assign roles to new user
+                    var roles = new List<string> { "User" }; // user role by default
+
+                    if (userViewModel.AminRoleCheckbox) // if admin role checkbox is checked
+                    {
+                        roles.Add("Admin");
+                    }
+
+                    identityResult = await userManager.AddToRolesAsync(identityUser, roles);
+
+                    if (identityResult is not null && identityResult.Succeeded)
+                    {
+                        return RedirectToAction("List", "AdminUsers");
+                    }
+                }
+            }
+
+            return View();
         }
     }
 }
--- Bloggie.Web/Models/ViewModels/UserViewModel.cs
+++ Bloggie.Web/Models/ViewModels/UserViewModel.cs
@@ -3,5 +3,10 @@
     public class UserViewModel
     {
         public List<User> Users { get; set; }
+
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool AminRoleCheckbox { get; set; }
     }
 }
